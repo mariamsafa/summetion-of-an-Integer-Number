5fbf213c6c212a18553b6926061e9544ebab6f2c
Mariam Safa <88893709+mariamsafa@users.noreply.github.com>
2021-10-26 00:06:34 +0600

    while loop to for loop

--- summetion of an Integer Number.cpp
+++ summetion of an Integer Number.cpp
@@ -1,10 +1,21 @@
-// summetion of an Integer Number
+// Summetion of an Integer Number
 #include<stdio.h>
 int main()
 {
 	int number, remainder, sum=0;
 	printf("Enter an Interger number : ");
 	scanf_s("%d", &number);
+
+	int i;
+	for (i=0; i <= number; i++)
+	{
+		remainder = number % 10;
+		number = number / 10;
+		sum = sum + remainder;
+	}
+	printf("\nSum of the Integer number is %d \n", sum);
+/*
+
 	while (number != 0)
 	{
 		remainder = number % 10;
@@ -12,4 +23,7 @@ int main()
 		sum = sum + remainder;
 	}
 	printf("\nSum of the Integer number is %d \n", sum);
+
+*/
+	return 0;
 }
